dc5d741173864208338c24c2bb3417d75ecc4610
aretmur <aret@murco.au>
2025-07-12 12:17:44 +1000

    Create Landing.jsx

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+// src/pages/Landing.jsx
+
+import Hero from '../components/Hero';
+import PricingPlans from '../components/PricingPlans';
+import Footer from '../components/Footer';
+
+export default function Landing() {
+  return (
+    <>
+      <Hero />
+      <PricingPlans />
+      <Footer />
+    </>
+  );
+}
