bb0e33502a9f2491fc85d0851ec7c5661e62a3fd
aretmur <aret@murco.au>
2025-07-12 14:41:12 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        'react-router-dom',
-        '@clerk/clerk-react',
-        '@supabase/supabase-js'
-      ]
-    }
-  }
+  plugins: [react()]
 })
