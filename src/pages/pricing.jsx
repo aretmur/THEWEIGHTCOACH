5fdd99dd7cacff5b987a9c8df7c35f150c34eb1d
aretmur <aret@murco.au>
2025-07-12 12:21:24 +1000

    Create pricing.jsx

---
+++ src/pages/pricing.jsx
@@ -0,0 +1,11 @@
+// src/pages/Pricing.jsx
+
+import PricingPlans from '../components/PricingPlans';
+
+export default function Pricing() {
+  return (
+    <div className="min-h-screen bg-white">
+      <PricingPlans />
+    </div>
+  );
+}
